e8164fe287ebf559f4fb87522c68f5679b034648
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-06 18:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let btn=document.getElementById("submit-btn");
+
+btn.addEventListener("click",()=>{
+	event.preventDefault();
+	let firstName=document.getElementById("fName").value;
+	let lastName=document.getElementById("lName").value;
+	let phoneNo=document.getElementById("phone").value;
+	let emailId=document.getElementById("email").value;
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNo} Email ID: ${emailId}`)
+})
+
